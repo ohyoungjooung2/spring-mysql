6163248e703dd62467816ab222e20220cbbb77d9
oyj <oyj@local>
2020-02-01 08:22:08 +0900

    ssh option changed

--- deliver.sh
+++ deliver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 KEY="$HOME/id_rsa"
+ST="-o StrictHostKeyChecking=no"
 #KCTL=$(which kubectl)
 echo 'TestingTesting'
 docker build . -t 10.1.0.7:3333/spring-mysql:1.0; docker images | grep spring-mysql
@@ -20,15 +21,15 @@ docker push 10.1.0.7:3333/spring-mysql:1.0; echo $?; echo test
 echo "Deployment of spring-mysql"
 if [[ -e spring-mysql-dp.yaml ]]
 then
-	scp -i $KEY -P 22 spring-mysql-dp.yaml vagrant@10.1.0.2:/home/vagrant/
-        ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl get deploy spring-mysql'
+	scp -i $KEY $ST -P 22 spring-mysql-dp.yaml vagrant@10.1.0.2:/home/vagrant/
+        ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl get deploy spring-mysql'
 	if [[ $? -eq 0 ]]
 	then
 		echo "spring-mysql deploy already exists , apply!"
-                ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl apply -f spring-mysql-dp.yaml'
+                ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl apply -f spring-mysql-dp.yaml'
         else
 		echo "spring-mysql deploy not exists , create!"
-	ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl create -f spring-mysql-dp.yaml'
+	ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl create -f spring-mysql-dp.yaml'
         fi
 else
 	echo "spring-mysql-dp.yaml not exists"
@@ -38,16 +39,16 @@ fi
 echo "Deployment of spring-mysql-svc"
 if [[ -e spring-mysql-svc.yaml ]]
 then
-	scp -i $KEY -P 22 spring-mysql-svc.yaml vagrant@10.1.0.2:/home/vagrant/
-        ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl get svc spring-mysql'
+	scp -i $KEY $ST -P 22 spring-mysql-svc.yaml vagrant@10.1.0.2:/home/vagrant/
+        ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl get svc spring-mysql'
 	if [[ $? -eq 0 ]]
 	then
 		echo "spring-mysql svc already exists , apply!"
-                ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl apply -f spring-mysql-svc.yaml'
+                ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl apply -f spring-mysql-svc.yaml'
 	else
 
 		echo "spring-mysql svc not exists , apply!"
-	        ssh -i $KEY -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl create -f spring-mysql-svc.yaml'
+	        ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 '/usr/bin/kubectl create -f spring-mysql-svc.yaml'
 	fi
 else
 	echo "spring-mysql-svc.yaml not exists"
@@ -69,11 +70,11 @@ TILTIME=300
 TILTIMECOMPLETE=$(($NOWTIME+$TILTIME))
 while true
 do
- ssh -i $KEY -p 22 vagrant@10.1.0.2 'curl http://10.1.0.3:32339/demo/all'
+ ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 'curl http://10.1.0.3:32339/demo/all'
  CHK
- ssh -i $KEY -p 22 vagrant@10.1.0.2 'curl http://10.1.0.4:32339/demo/all'
+ ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 'curl http://10.1.0.4:32339/demo/all'
  CHK
- ssh -i $KEY -p 22 vagrant@10.1.0.2 'curl http://10.1.0.4:32339/demo/all'
+ ssh -i $KEY $ST -p 22 vagrant@10.1.0.2 'curl http://10.1.0.4:32339/demo/all'
  CHK
  CURRENTIME=$(date +%s)
  DURATION_SEC=$(($CURRENTIME-$NOWTIME))
